b048791f785f80461149c27a75ffff69eb7dbec6
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2024-05-28 08:21:31 -0300

    Add files via upload

---
+++ site-do-colegio-main/site-do-colegio-main/audio.js
@@ -0,0 +1,22 @@
+function tocar(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+const ListaTeclas = document.querySelectorAll('.tecla');
+
+for (let Contador = 0; Contador < ListaTeclas.length; Contador++) {
+    const botão = ListaTeclas[Contador];
+    const tecla = botão.classList[1];
+    const idAudio = `#som_${tecla}`;
+    console.log (idAudio);
+    botão.onclick = function () {
+        tocar(idAudio);
+    }
+    botão.onkeydown = function (Evento) {
+        if (Evento.code === 'Space' || Evento.code === 'Enter') {
+            botão.classList.add('ativa');
+        }
+    }
+    botão.onkeyup = function () {
+        botão.classList.remove('ativa');
+    }
+}
