d26216928f5ef58f2c9be2e6ea21bc9d733c8eb5
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2024-06-11 08:37:36 -0300

    bb

--- main.js
+++ main.js
@@ -14,15 +14,6 @@ function texto(){
   
   }
   
-  alert('Bem vindo');
-
-let nomeUsuario = '';
-let texto = document.querySelector('span');
-
-while (nomeUsuario == ''){
- nomeUsuario = prompt('qual é o seu nome?');
-}
-
 if(nomeUsuario == null){
    texto.textContent = 'sr(a)'; 
 } else{
