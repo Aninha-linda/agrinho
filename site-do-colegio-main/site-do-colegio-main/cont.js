40ff04990c278ab476ed48823b941b9a6e874518
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2024-06-11 08:26:32 -0300

    l

--- site-do-colegio-main/site-do-colegio-main/cont.js
+++ site-do-colegio-main/site-do-colegio-main/cont.js
@@ -2,8 +2,6 @@ const botoes = document.querySelectorAll('.Objetivo');
 const texto = document.querySelectorAll('.aba-conteudo');
 //botoes.lenght     
 
-@media screem and (min-width: 1024px) {/* tablet}
-
 for (let i = 0; i < botoes.length; i++) {
     console.log(i);
     botoes[i].onclick = function () {
@@ -17,18 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 }
 
-const contadores = document.querySelectorAll('.contador');
-const TempoObjetivo1 = new Date('2024-05-17T00:00:00');
-const TempoObjetivo2 = new Date('2024-05-21T00:00:00');
-const TempoObjetivo3 = new Date('2024-07-05T00:00:00');
-const TempoObjetivo4 = new Date('2024-12-13T00:00:00');
-let Tempos = [ TempoObjetivo1, TempoObjetivo2, TempoObjetivo3, TempoObjetivo4 ];
-for (i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(Tempos[i]);
-}
-
-
-function calculaTempo(TempoObjetivo) {
+/* function calculaTempo(TempoObjetivo) {
 
     let TempoAtual = new Date();
     let TempoFinal = TempoObjetivo - TempoAtual;
@@ -42,4 +29,4 @@ function calculaTempo(TempoObjetivo) {
     horas %= 24;
 
     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
-}
+} /*
